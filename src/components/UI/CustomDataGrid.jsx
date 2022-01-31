cbaf10925e394a7f8fc75a9e837bfedca3082bff
GEAR-2012 <sandor.tudja@gmail.com>
2022-01-31 10:37:57 +0000

    fix page size change issue on data grid component

--- src/components/UI/CustomDataGrid.jsx
+++ src/components/UI/CustomDataGrid.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 
 const CustomDataGrid = ({ columns, rows, action }) => {
+  const [pageSize, setPageSize] = useState(25);
+
   const handleRowClick = (params) => {
     action(params.id);
+  };
+
+  const handlePageSizeChange = (num) => {
+    setPageSize(num);
   };
 
   return (
     <div style={{ height: 400 }}>
       <DataGrid
-        rowHeight={25}
+        rowHeight={41}
         onRowClick={handleRowClick}
         sx={{ my: 5 }}
         columns={columns}
         rows={rows}
-        pageSize={25}
+        pageSize={pageSize}
+        onPageSizeChange={handlePageSizeChange}
         disableSelectionOnClick
         components={{
           Toolbar: GridToolbar,
